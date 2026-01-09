d1311fcd89673ec039eb07f37d7bb820ae8d5714
Flavio Gavino Rios Nieto <frios@nelson.local>
2026-01-09 16:51:45 +0100

    creacion de api de tienda

---
+++ 01_primeros_pasos/01_primeros_pasos/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numero = 42;
+string nombre = "Juan";
+bool esVerdadero = true;
+
+Console.WriteLine($"Número: {numero}, Nombre: {nombre}, Es Verdadero: {esVerdadero}");
+Console.WriteLine("Presiona Enter para salir...");
+Console.ReadLine();
---
+++ ServidorTiendaDotNet/ServidorTiendaDotNet/Models/Flor.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ServidorTiendaDotNet.Models
+{
+    public class Flor
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("nombre")]
+        public string Nombre { get; set; }
+        [JsonPropertyName("descripcion")]
+        public string Descripcion { get; set; }
+        [JsonPropertyName("precio")]
+        public decimal Precio { get; set; }
+        [JsonPropertyName("activo")]
+        public bool Activo { get; set; }
+    }
+}
