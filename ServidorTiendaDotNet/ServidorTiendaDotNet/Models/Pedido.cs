d89182b3e13bae9287c7bb417196d6aa4fb91a8c
Flavio Gavino Rios Nieto <frios@nelson.local>
2026-01-16 16:01:06 +0100

    feat: aplicar validaciones en Pedido

--- ServidorTiendaDotNet/ServidorTiendaDotNet/Models/Pedido.cs
+++ ServidorTiendaDotNet/ServidorTiendaDotNet/Models/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ServidorTiendaDotNet.Models
 {
@@ -6,12 +7,19 @@ namespace ServidorTiendaDotNet.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Nombre no puede estar vacío")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ,. ]{3,20}$")]
         [JsonPropertyName("cliente")]
         public String Cliente { get; set; }
 
+        
+        [Required(ErrorMessage = "Tarjeta no puede estar vacio")]
+        [RegularExpression("^[0-9 ]{16-22}$")]
         [JsonPropertyName("numeroTarjeta")]
         public string NumeroTarjeta { get; set; }
 
+        [MinLength(10, ErrorMessage = "Dirección debe tener al menos 10 caracteres")]
+        [MaxLength(100, ErrorMessage = "Dirección no puede tener más de 100 caracteres")]
         [JsonPropertyName("direccionEnvio")]
         public string DireccionEnvio { get; set; } = string.Empty;
 
